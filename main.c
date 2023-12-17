0c219e284b2e78172c4163c5387b027e7f43d188
ca1c <gegs921@gmail.com>
2023-12-16 23:32:44 -0800

    work in progress for finding a solution to store all possible colors

--- main.c
+++ main.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <jpeglib.h>
+#include <stdbool.h>
+#include <math.h>
 
 void setPixelColor(unsigned char * pixel, int r, int g, int b) {
     pixel[0] = r;
     pixel[1] = g;
     pixel[2] = b;
+}
+
+bool colorsContainsColor(unsigned char colors[][3], unsigned char * color, int colorsLength) {
+    for(int i = 0; i < colorsLength; i++) {
+        if((colors[i][0] == color[0]) 
+        && (colors[i][1] == color[1]) 
+        && (colors[i][2] == color[2])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void setColorsElement(unsigned char colors[], unsigned char * color) {
+    // printf("%d, %d, %d\n", color[0], color[1], color[2]);
+    colors[0] = color[0];
+    colors[1] = color[1];
+    colors[2] = color[2];
+}
+
+int findColorIndex(unsigned char colors[][3], unsigned char * color, int colorsLength) {
+    for(int i = 0; i < colorsLength; i++) {
+        if((colors[i][0] == color[0]) 
+        && (colors[i][1] == color[1]) 
+        && (colors[i][2] == color[2])) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main(int argc, char ** argv) {
@@ -64,9 +95,46 @@ int main(int argc, char ** argv) {
 
     jpeg_start_compress(&output_cinfo, TRUE);
 
+    // unsigned char colors[255 * 255][3];
+
+
+    /**
+     * Plan for later:
+     * rewrite this instead of using nested arrays
+     * make an array of structs
+     * // Define the size of the array
+    size_t array_size = 255 * 255 * 255;
+
+    // Allocate memory for the array of struct color
+    struct color *colorArray = (struct color *)malloc(array_size * sizeof(struct color))
+    */
+
+
+    size_t colors_size = 255 * 255 * 255;
+    unsigned char ** colors = (unsigned char *)malloc(colors_size * sizeof(unsigned char));
+
+    for(int i = 0; i < 255 * 255 * 255; i++) {
+        colors[i] = malloc(sizeof(unsigned char) * 3);
+    }
+
+    unsigned char * counts = (unsigned char *)malloc(colors_size * sizeof(unsigned char));
+    int j = 0;
+
     while(cinfo.output_scanline < cinfo.output_height) {
         (void) jpeg_read_scanlines(&cinfo, buffer, 1);
         // put_scanline_someplace(buffer[0], row_stride);
+
+        for(int i = 0; i < row_stride; i+= cinfo.output_components) {
+            printf("%d\n", j);
+            if(!colorsContainsColor(colors, &(buffer[0][i]), j)) {
+                setColorsElement(colors[j], &(buffer[0][i]));
+                j++;
+            }
+            else {
+                counts[findColorIndex(colors, &(buffer[0][i]), j)]++;
+            }
+        }
+
         for(int i = 0; i < row_stride; i += cinfo.output_components) {
             setPixelColor(&(buffer[0][i]), 0, 0, 255);
         }

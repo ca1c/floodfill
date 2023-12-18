50d857b62fb79ab1ec8358562158e8b442abd04f
ca1c <gegs921@gmail.com>
2023-12-17 16:38:14 -0800

    find all unique colors in picture

--- main.c
+++ main.c
@@ -4,35 +4,43 @@
 #include <stdbool.h>
 #include <math.h>
 
+struct color {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+};
+
 void setPixelColor(unsigned char * pixel, int r, int g, int b) {
     pixel[0] = r;
     pixel[1] = g;
     pixel[2] = b;
 }
 
-bool colorsContainsColor(unsigned char colors[][3], unsigned char * color, int colorsLength) {
+bool colorsContainsColor(struct color * colors, unsigned char * color, int colorsLength) {
     for(int i = 0; i < colorsLength; i++) {
-        if((colors[i][0] == color[0]) 
-        && (colors[i][1] == color[1]) 
-        && (colors[i][2] == color[2])) {
+        if((colors[i].r == color[0]) 
+        && (colors[i].g == color[1]) 
+        && (colors[i].b == color[2])) {
             return true;
         }
     }
     return false;
 }
 
-void setColorsElement(unsigned char colors[], unsigned char * color) {
+struct color setColorsElement(struct color currColor, unsigned char * color) {
     // printf("%d, %d, %d\n", color[0], color[1], color[2]);
-    colors[0] = color[0];
-    colors[1] = color[1];
-    colors[2] = color[2];
+    currColor.r = color[0];
+    currColor.g = color[1];
+    currColor.b = color[2];
+
+    return currColor;
 }
 
-int findColorIndex(unsigned char colors[][3], unsigned char * color, int colorsLength) {
+int findColorIndex(struct color * colors, unsigned char * color, int colorsLength) {
     for(int i = 0; i < colorsLength; i++) {
-        if((colors[i][0] == color[0]) 
-        && (colors[i][1] == color[1]) 
-        && (colors[i][2] == color[2])) {
+        if((colors[i].r == color[0]) 
+        && (colors[i].g == color[1]) 
+        && (colors[i].b == color[2])) {
             return i;
         }
     }
@@ -95,29 +103,10 @@ int main(int argc, char ** argv) {
 
     jpeg_start_compress(&output_cinfo, TRUE);
 
-    // unsigned char colors[255 * 255][3];
-
-
-    /**
-     * Plan for later:
-     * rewrite this instead of using nested arrays
-     * make an array of structs
-     * // Define the size of the array
     size_t array_size = 255 * 255 * 255;
 
-    // Allocate memory for the array of struct color
-    struct color *colorArray = (struct color *)malloc(array_size * sizeof(struct color))
-    */
-
-
-    size_t colors_size = 255 * 255 * 255;
-    unsigned char ** colors = (unsigned char *)malloc(colors_size * sizeof(unsigned char));
-
-    for(int i = 0; i < 255 * 255 * 255; i++) {
-        colors[i] = malloc(sizeof(unsigned char) * 3);
-    }
-
-    unsigned char * counts = (unsigned char *)malloc(colors_size * sizeof(unsigned char));
+    struct color *colorsArr = (struct color *)malloc(array_size * sizeof(struct color));
+    int * colorsCount = (int *)malloc(array_size * sizeof(int));
     int j = 0;
 
     while(cinfo.output_scanline < cinfo.output_height) {
@@ -126,12 +115,13 @@ int main(int argc, char ** argv) {
 
         for(int i = 0; i < row_stride; i+= cinfo.output_components) {
             printf("%d\n", j);
-            if(!colorsContainsColor(colors, &(buffer[0][i]), j)) {
-                setColorsElement(colors[j], &(buffer[0][i]));
+            int colorIndex;
+            if(!colorsContainsColor(colorsArr, &(buffer[0][i]), j)) {
+                colorsArr[j] = setColorsElement(colorsArr[j], &(buffer[0][i]));
                 j++;
             }
-            else {
-                counts[findColorIndex(colors, &(buffer[0][i]), j)]++;
+            else if((colorIndex = findColorIndex(colorsArr, &(buffer[0][i]), j)) != -1) {
+                colorsCount[j]++;
             }
         }
 
